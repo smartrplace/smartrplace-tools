fbf1d988a3c1761dfd67fb79dbe15d5009a0fc1a
cnoelle <christoph.noelle@iee.fraunhofer.de>
2018-11-16 22:16:52 +0100

    servlet tools: build fixed

--- src/servlet-tools/smartrplace-servlet-context-test/src/test/java/org/smartrplace/tools/servlet/context/test/ContextTest.java
+++ src/servlet-tools/smartrplace-servlet-context-test/src/test/java/org/smartrplace/tools/servlet/context/test/ContextTest.java
@@ -11,7 +11,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.AllPermission;
 import java.security.Permission;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Dictionary;
 import java.util.Hashtable;
